723030c7c7ce14ffcf957f55f04f9cc0e69dc1b5
ArjunReddy3103 <arjunprojects3103@gmail.com>
2025-07-24 15:42:42 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # -------------------- CORS Middleware --------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://zesty-halva-749f5e.netlify.app"],  # ✅ Set specific domain in prod
+    allow_origins=["funny-beijinho-d8d7ce.netlify.app"],  # ✅ Set specific domain in prod
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
