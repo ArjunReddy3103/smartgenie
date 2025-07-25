fc5145f3caa016af5f1a039040ae0770f91ef43a
ArjunReddy3103 <arjunprojects3103@gmail.com>
2025-07-25 12:19:16 +0530

    Update firebase.py

--- backend/firebase.py
+++ backend/firebase.py
@@ -12,14 +12,3 @@ def init_firebase():
         cred = credentials.Certificate(cred_path)
         firebase_app = firebase_admin.initialize_app(cred)
     return firestore.client()
-
-def store_report_data(company_name, date_range, summary):
-    db = init_firebase()
-    doc_ref = db.collection("reports").document(company_name).collection("weekly_reports").document(date_range)
-
-    doc_ref.set({
-        "company": company_name,
-        "date_range": date_range,
-        "summary": summary
-    })
-    print(f"[FIRESTORE] ✅ Data saved for {company_name} – {date_range}")
